e334bc9965634a649b8be30b47733902b6812e0d
Trouvaille0198 <1375911011@qq.com>
2020-12-15 16:10:18 +0800

    离散摸鱼后缀表达式

--- MyOwnRealization/LinkStack/表达式求值/SuffixExpreesion.h
+++ MyOwnRealization/LinkStack/表达式求值/SuffixExpreesion.h
@@ -1,7 +1,7 @@
 #ifndef RPN
 #define RPN
-#include "D:\repo\DataStructure\MyOwnRealization\SeqList\SeqList.h"
-#include "D:\repo\DataStructure\MyOwnRealization\SeqStack\SeqStack.h"
+#include "../../SeqList/SeqList.h"
+#include "../LinkStack.h"
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -18,32 +18,78 @@ SeqList<string> InputSufxExpr()
         cin >> str[count++];
     }
 
-    /*
-    另一种输入实现
-    char c;
-    while ((c = getchar()) != '#')
-    {
-        ch[count++] = c;
-    }
-    */
-
     SeqList<string> SufxExpr(str, count - 1);
     return SufxExpr;
+}
+
+bool IsNum(string str)
+{
+    stringstream sin(str);
+    double d;
+    char c;
+    if (!(sin >> d)) //表示把sin转换成double的变量（其实对于int和float型的都会接收）
+        return false;
+    if (sin >> c) //此部分用于检测错误输入中，数字加字符串的输入形式
+        return false;
+    return true;
+}
+
+bool IsOperator(string str)
+{
+    if (str == "+" || str == "-" || str == "*" || str == "/" || str == "^")
+        return true;
+    else
+    {
+        return false;
+    }
+}
+
+double Calculate(double first, double second, string opr)
+{
+    double result;
+    switch (opr[0])
+    {
+    case '+':
+        result = first + second;
+        break;
+    case '-':
+        result = first - second;
+        break;
+    case '*':
+        result = first * second;
+        break;
+    case '/':
+        result = first / second;
+        break;
+    case '^':
+        result = pow(first, second);
+        break;
+    }
+    return result;
 }
 
 double SufxExprCal(SeqList<string> &SufxExpr)
 //后缀表达式计算结果
 {
-    double result, first, second;
-    SeqList<double> operand; //操作数栈
+    double result = 0, first, second;
+    LinkStack<double> Operand; //操作数栈
     for (int i = 1; i <= SufxExpr.GetLength(); i++)
     {
-        while (isdigit(SufxExpr.GetElem(i)) || SufxExpr.GetElem(i) == ".")
+        if (IsNum(SufxExpr.GetElem(i))) //如果是操作数
         {
-
-            i++;
+            Operand.PushElem(stod(SufxExpr.GetElem(i)));
+        }
+        else
+        {
+            second = Operand.TopElem(); //取第二操作数
+            Operand.PopElem();
+            first = Operand.TopElem(); //取第一操作数
+            Operand.PopElem();
+            Operand.PushElem(Calculate(first, second, SufxExpr.GetElem(i)));
+            //两操作数出栈计算，将结果重新入栈
         }
     }
+    return result;
 }
 
 #endif
--- MyOwnRealization/LinkStack/表达式求值/Test.cpp
+++ MyOwnRealization/LinkStack/表达式求值/Test.cpp
@@ -6,5 +6,7 @@ int main()
     SeqList<string> SufxExpr = InputSufxExpr();
     cout << SufxExpr.GetLength() << endl;
     SufxExpr.DisplayList();
+    double re = SufxExprCal(SufxExpr);
+    cout << re;
     system("pause");
 }
