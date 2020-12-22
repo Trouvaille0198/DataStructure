3dc1f29db72c22875ac9a1f84fd3834422f9f4c1
Trouvaille0198 <1375911011@qq.com>
2020-12-22 08:16:51 +0800

    add T8

--- Work2/T8/Test.cpp
+++ Work2/T8/Test.cpp
@@ -1,7 +1,28 @@
 #include <bits/stdc++.h>
+#include "../../MyOwnRealization/SeqStack/SeqStack.h"
 using namespace std;
+
+void Test()
+{
+    int sum = 0;
+    SeqStack<int> s;
+    int x;
+    do
+    {
+        cin >> x;
+        s.PushElem(x);
+    } while (x != 0);
+    while (!s.IsEmpty())
+    {
+        x = s.TopElem();
+        s.PopElem();
+        sum += x;
+        cout << "The sum is: " << sum << endl;
+    }
+}
 
 int main()
 {
+    Test();
     system("pause");
 }
