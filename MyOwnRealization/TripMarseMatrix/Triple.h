de48c4860bdf6aaaace2953aaac627c57ba4de63
Trouvaille0198 <1375911011@qq.com>
2020-12-28 10:07:59 +0800

    2020/12/28 10:08 push

---
+++ MyOwnRealization/TripMarseMatrix/Triple.h
@@ -0,0 +1,16 @@
+#ifndef TRIPLE_H
+#define TRIPLE_H
+#include <bits/stdc++.h>
+using namespace std;
+
+template <class DataType>
+class Triple
+{
+    int _row, _col;
+    DataType _value;
+
+    Triple(){};
+    Triple(int row, int col, DataType value) _row(row), _col(col), _value(value){};
+};
+
+#endif
