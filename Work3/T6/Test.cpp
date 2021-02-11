a28610087784608dd4902a9ebc76e646e808ca70
Trouvaille0198 <1375911011@qq.com>
2021-02-11 16:01:16 +0800

    unknown update

--- Work3/T6/Test.cpp
+++ Work3/T6/Test.cpp
@@ -4,28 +4,30 @@ using namespace std;
 
 int main()
 {
-    TriSparseMatrix<int> t1(50, 50, 100);
-    TriSparseMatrix<int> t2(50, 50, 100);
-    /* t1.SetElem(1, 1, 50);
-    t1.SetElem(1, 2, 51);
-    cout << t1.GetElem(1, 1) << ", " << t1.GetElem(1, 2) << endl;
-    t1.SetElem(1, 1, 0);
-    t1.SetElem(1, 2, 55);
-    cout << t1.GetElem(1, 1) << ", " << t1.GetElem(1, 2) << endl;
-    cout << t1.GetNum() << endl; */
-    t1.SetElem(0, 0, 5);
-    t1.SetElem(0, 2, 5);
-    t1.SetElem(1, 2, 5);
-    t1.SetElem(3, 4, 5);
-    t1.SetElem(9, 9, 100);
+	TriSparseMatrix<int> t1(50, 50, 100);
+	TriSparseMatrix<int> t2(50, 50, 100);
 
-    t2.SetElem(0, 0, 6);
-    t2.SetElem(0, 4, 6);
-    t2.SetElem(2, 4, 6);
-    t2.SetElem(3, 4, -5);
-    TriSparseMatrix<int> tt(50, 50, 100);
-    tt = t1 + t2;
-    tt.Display();
-    cout << tt.GetNum() << endl;
-    system("pause");
+	/* t1.SetElem(1, 1, 50);
+	 * t1.SetElem(1, 2, 51);
+	 * cout << t1.GetElem(1, 1) << ", " << t1.GetElem(1, 2) << endl;
+	 * t1.SetElem(1, 1, 0);
+	 * t1.SetElem(1, 2, 55);
+	 * cout << t1.GetElem(1, 1) << ", " << t1.GetElem(1, 2) << endl;
+	 * cout << t1.GetNum() << endl; */
+	t1.SetElem(0, 0, 5);
+	t1.SetElem(0, 2, 5);
+	t1.SetElem(1, 2, 5);
+	t1.SetElem(3, 4, 5);
+	t1.SetElem(9, 9, 100);
+
+	t2.SetElem(0, 0, 6);
+	t2.SetElem(0, 4, 6);
+	t2.SetElem(2, 4, 6);
+	t2.SetElem(3, 4, -5);
+	TriSparseMatrix<int> tt(50, 50, 100);
+
+	tt = t1 + t2;
+	tt.Display();
+	cout << tt.GetNum() << endl;
+	system("pause");
 }
--- test.cpp
+++ test.cpp
@@ -3,7 +3,9 @@ using namespace std;
 
 int main()
 {
-        int a = 1;
-        cout << a << endl;
-        system("pause");
+	cout << "Hello world!" << endl;
+	int a = 1;
+
+	cout << a << endl;
+	system("pause");
 }
