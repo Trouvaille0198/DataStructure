360d3e4f7f6731829f329bdda1d18ac1df839311
Tyeah <1375911011@qq.com>
2021-03-03 08:48:25 +0800

    finish work4

--- Work4/T7 OrderBinTree/T7.cpp
+++ Work4/T7 OrderBinTree/T7.cpp
@@ -1,6 +1,6 @@
 //找出A[i],A[j]最近的共同祖先
 
-#include "orderBinaryTree1.h"
+#include "orderBinaryTree.h"
 using namespace std;
 
 //             0
@@ -13,11 +13,11 @@ int main()
   Node<int> *TreeNode1;
   CreateNodeArray(TreeNode1);
   BinaryTree<int> Tree(10, TreeNode1);
-  Tree.showper();
+  //Tree.showper();
   int a, b;
   while (cin >> a >> b)
   {
-    Tree.Find(a, b);
+    Tree.FindParent(a, b);
   }
   system("pause");
 }
--- Work4/T7 OrderBinTree/orderBinaryTree.h
+++ Work4/T7 OrderBinTree/orderBinaryTree.h
@@ -1,105 +1,98 @@
 //顺序二叉树
-#include <iostream>
-#include <iomanip>
+#include <bits/stdc++.h>
 using namespace std;
 
-template <class ElemType>
+template <class T>
 class Node
 {
 public:
-    int _RefValue; //标记节点类型
-    ElemType data;
-    Node() //默认构造函数
+    int Ref; //标记节点类型
+    T data;
+    Node() //默认构造
     {
     }
-    Node(int r, const ElemType &t) //非完美二叉树的结点
+    Node(int r, const T &t) //非完全二叉树的结点
     {
-        _RefValue = r;
+        Ref = r;
         data = t;
     }
-    Node(const ElemType &t) //完美二叉树的结点
+    Node(const T &t) //完全二叉树的结点
     {
-        _RefValue = 0;
+        Ref = 0;
         data = t;
     }
     Node operator=(Node *a)
     {
-        _RefValue = a->_RefValue;
+        Ref = a->Ref;
         data = a->data;
     }
 };
 
-template <class ElemType>
-void CreateNodeArray(Node<ElemType> *&a)
+template <class T>
+void CreateNodeArray(Node<T> *&a)
 {
-    Node<ElemType> *List;
-    List = new Node<ElemType>[10];
-    List[0] = new Node<ElemType>(0, 10);
-    List[1] = new Node<ElemType>(1, 9);
-    List[2] = new Node<ElemType>(2, 8);
-    List[3] = new Node<ElemType>(1, 7);
-    List[4] = new Node<ElemType>(2, 6);
-    List[5] = new Node<ElemType>(1, 5);
-    List[6] = new Node<ElemType>(2, 4);
-    List[7] = new Node<ElemType>(1, 3);
-    List[8] = new Node<ElemType>(2, 2);
-    List[9] = new Node<ElemType>(1, 1);
+    Node<T> *List;
+    List = new Node<T>[10];
+    List[0] = new Node<T>(0, 0);
+    List[1] = new Node<T>(1, 1);
+    List[2] = new Node<T>(2, 2);
+    List[3] = new Node<T>(1, 3);
+    List[4] = new Node<T>(2, 4);
+    List[5] = new Node<T>(1, 5);
+    List[6] = new Node<T>(2, 6);
+    List[7] = new Node<T>(1, 7);
+    List[8] = new Node<T>(2, 8);
+    List[9] = new Node<T>(1, 9);
     a = List;
 }
 
-template <class ElemType>
+template <class T>
 class BinaryTree
 {
 protected:
-    int NodeNum;
-    int RankNum;
-    Node<ElemType> *DataArray; //里面全是指针
-    int *_RefValueArray;
+    int _nodeNum;        //节点数
+    int _rankNum;        //层数
+    Node<T> *_dataArray; //里面全是指针
+    int *_refArray;
 
 public:
-    BinaryTree(int nodenum, Node<ElemType> *data);              //完美二叉树
-    BinaryTree(int nodenum, Node<ElemType> *data, int ranknum); //非完美二叉树
+    BinaryTree(int nodeNum, Node<T> *data);              //完全二叉树
+    BinaryTree(int nodeNum, Node<T> *data, int rankNum); //非完全二叉树
     void showper();
     void Find(int i, int j);
+    void FindParent(int a, int b);
 };
 
-template <class ElemType>
-BinaryTree<ElemType>::BinaryTree(int nodenum, Node<ElemType> *data) //创建完美二叉树
+template <class T>
+BinaryTree<T>::BinaryTree(int nodeNum, Node<T> *data) //创建完美二叉树
 {
-    NodeNum = nodenum;
-    int ranknum = 0;
-    for (int j = 1; (nodenum = nodenum - j) > 0; j = j * 2)
+    _nodeNum = nodeNum;
+    int rankNum = 0;
+    for (int j = 1; (nodeNum = nodeNum - j) > 0; j = j * 2)
+    // 计算层数
     {
-        ranknum++;
+        rankNum++;
     }
-    ranknum++;
-    RankNum = ranknum;
-    /*
-    int n;
-    int s=0;
-    for(int i=1,n=1; i<=ranknum; i++){
-        s = s+n;
-        n = n*2;
-    }
-    */
-    DataArray = new Node<ElemType>[NodeNum];
-    for (int i = 0; i < NodeNum; i++)
+    rankNum++;
+    _rankNum = rankNum;
+
+    _dataArray = new Node<T>[_nodeNum];
+    for (int i = 0; i < _nodeNum; i++)
+    // 依次将元素放入数组
     {
-        DataArray[i] = data[i];
+        _dataArray[i] = data[i];
     }
 }
 
-template <class ElemType>
-void BinaryTree<ElemType>::showper()
+template <class T>
+void BinaryTree<T>::showper()
 {
     int temp = 0;
-    for (int i = 0; i < RankNum; i++)
+    for (int i = 0; i < _rankNum; i++)
     {
-        cout << setw(25) << ""
-             << "The " << i << " floor:" << setw(5) << "";
         if (i == 0)
         {
-            cout << DataArray[i].data << endl;
+            cout << _dataArray[i].data << endl;
             temp++;
         }
         else
@@ -109,8 +102,8 @@ void BinaryTree<ElemType>::showper()
                 for (int j = 0; j < 2 * i; j++)
                 { //决定每一层的输出个数
                     temp++;
-                    cout << DataArray[temp - 1].data << "  ";
-                    if (temp == NodeNum)
+                    cout << _dataArray[temp - 1].data << "  ";
+                    if (temp == _nodeNum)
                     {
                         break;
                     }
@@ -121,12 +114,12 @@ void BinaryTree<ElemType>::showper()
     }
 }
 
-template <class ElemType>                     //寻找下标i j的共同祖先 输出下标位置
-void BinaryTree<ElemType>::Find(int a, int b) //建立在完美二叉树
+template <class T>                     //寻找下标i j的共同祖先 输出下标位置
+void BinaryTree<T>::Find(int a, int b) //使用完全二叉树
 {
     int ta = a, tb = b;
     int ra, rb; //判断下标所在位置是左边还是右边
-    for (int i = 0; i < RankNum; i++)
+    for (int i = 0; i < _rankNum; i++)
     {
         int j = 1;
         for (int l = 0; l < i; l++)
@@ -148,7 +141,7 @@ void BinaryTree<ElemType>::Find(int a, int b) //建立在完美二叉树
             break;
         }
     }
-    for (int i = 0; i < RankNum; i++)
+    for (int i = 0; i < _rankNum; i++)
     { //找另一个下标的位置
         int j = 1;
         for (int l = 0; l < i; l++)
@@ -172,18 +165,36 @@ void BinaryTree<ElemType>::Find(int a, int b) //建立在完美二叉树
     }
     if (ra != rb)
     { //若左右不同，共同祖先一定是根
-        cout << setw(25) << ""
-             << "共同祖先:根 0" << endl;
+        cout << "共同祖先: 0" << endl;
         return;
     }
     if (a < b)
     { //若相同，一定是层数靠前的一个的父亲
-        cout << setw(25) << ""
-             << "共同祖先:" << (a - 1) / 2 << endl;
+        cout << "共同祖先: " << (a - 1) / 2 << endl;
     }
     else
     {
-        cout << setw(25) << ""
-             << "共同祖先:" << (b - 1) / 2 << endl;
+        cout << "共同祖先:" << (b - 1) / 2 << endl;
     }
+}
+
+template <class T>
+void BinaryTree<T>::FindParent(int a, int b)
+//寻找下标a,b的共同祖先 输出下标位置
+{
+    a = a + 1;
+    b = b + 1;
+    while ((a) / 2 != (b) / 2)
+    {
+        if (a < b)
+            b /= 2;
+        else
+            a /= 2;
+    }
+    int result = a / 2 - 1;
+    if (result < 0)
+        cout << "共同祖先: " << 0 << endl;
+
+    else
+        cout << "共同祖先: " << result << endl;
 }
