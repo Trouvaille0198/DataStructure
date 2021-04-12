199f953cee7dd2e045772f4e2f2f557ee37e7e5a
Tyeah <1375911011@qq.com>
2021-04-12 09:59:02 +0800

    add Adj

---
+++ Realization/Graph/AdjMatrixUndiGraph.h
@@ -0,0 +1,14 @@
+#ifndef ADJ_MATRIX_UNDI_GRAPH
+#define ADJ_MATRIX_UNDI_GRAPH
+#include <bits/stdc++.h>
+#include "../SeqList/SeqList.h"
+using namespace std;
+
+template <class T>
+class AdjMatrixUndiGraph
+{
+protected:
+    int
+};
+
+#endif
