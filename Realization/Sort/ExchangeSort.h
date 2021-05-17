e3f1ee52d88a0000c8e48848422c3d6f81c61818
Tyeah <1375911011@qq.com>
2021-05-17 10:45:07 +0800

    init sort

---
+++ Realization/Sort/ExchangeSort.h
@@ -0,0 +1,25 @@
+#include "../SeqList/SeqList.h"
+#include "../Tree/BinaryTree/BinaryTree.h"
+#include <bits/stdc++.h>
+using namespace std;
+
+// Exchange sort
+template <class T>
+SeqList<T> BubbleSort(SeqList<T> list) {}
+
+template <class T>
+SeqList<T> QuickSort(SeqList<T> list) {}
+
+//Insertion sort
+template <class T>
+SeqList<T> StraightInsertionSort(SeqList<T> list) {}
+
+template <class T>
+SeqList<T> BinaryInsertionSort(SeqList<T> list) {}
+
+template <class T>
+SeqList<T> ShellSort(SeqList<T> list) {}
+
+// Selection sort
+template <class T>
+SeqList<T> SimpleSelectionSort(SeqList<T> list) {}
